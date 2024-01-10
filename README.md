26815f2e2de098fd1cf4080b286d7aeb51a2e6dd
Jo <134626490+Jcait@users.noreply.github.com>
2024-01-10 12:10:30 +0000

    set up divs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-sketch
+
+https://www.theodinproject.com/lessons/foundations-etch-a-sketch
---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container')
+const div = document.createElement('div')
+const p = document.createElement('p')
+let i = 16
+
+// 
+ while (i >0) {
+    let div = document.createElement('div')
+    container.append(div)
+    i--
+ }
